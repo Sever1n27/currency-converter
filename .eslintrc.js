79757ed3515d15d9beba23e8561a85b88e486f0b
Severin Gritsenko <severin@rnd.stcnet.ru>
2020-10-25 14:40:59 +0300

    prettify

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,6 @@ module.exports = {
     ],
     rules: {
         '@typescript-eslint/no-var-requires': 'off',
-        "@typescript-eslint/explicit-function-return-type": "off"
+        '@typescript-eslint/explicit-function-return-type': 'off',
     },
 };
